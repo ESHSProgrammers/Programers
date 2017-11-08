4c4e68910103acbf0540ab64ce060cdb74d6704a
Nolan Williams <epsnwilliams02@epsne.org>
2017-11-08 17:12:05 -0600

    por relidade ahora

---
+++ Team C/In Progress/Plan B Outline.c
@@ -0,0 +1,87 @@
+
+task main()
+
+//this is plan B for the left side
+
+//move forward
+
+//turn 90 degrees to the right
+
+//move to standing goal
+
+//lift up
+
+//position claw
+
+//place starting cone on goal
+
+//lower lift
+
+//back up
+
+//rotate 90 degrees to the left
+
+//move toward next cone
+
+//position claw
+
+//grab the cone
+
+//back up near standing goal
+
+//rotate 90 degrees to the right
+
+//move forward
+
+//lift the lift
+
+//put cone on standing goal
+
+//likely end of program
+
+
+
+
+
+
+
+
+//RIGHT SIDE PLAN
+
+//move forward
+
+//turn 90 degrees to the left
+
+//move to standing goal
+
+//lift up
+
+//position claw
+
+//place starting cone on goal
+
+//lower lift
+
+//back up
+
+//rotate 90 degrees to the right
+
+//move toward next cone
+
+//position claw
+
+//grab the cone
+
+//back up near standing goal
+
+//rotate 90 degrees to the left
+
+//move forward
+
+//lift the lift
+
+//put cone on standing goal
+
+//likely end of program
+
+}
